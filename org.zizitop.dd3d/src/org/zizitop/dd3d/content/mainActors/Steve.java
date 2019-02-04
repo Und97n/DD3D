41a633e0dd32d2a62af268b1cd429439f9f6b553
ZizitopU <und97n@gmail.com>
2019-02-04 12:54:39 +0200

    Collision detection and some optimizations

---
+++ org.zizitop.dd3d/src/org/zizitop/dd3d/content/mainActors/Steve.java
@@ -0,0 +1,11 @@
+package org.zizitop.dd3d.content.mainActors;
+
+/**
+ * <br><br>
+ * Created 02.02.19 19:29
+ *
+ * @author Zizitop
+ */
+public class Steve {
+
+}
---
+++ org.zizitop.game/src/org/zizitop/game/sprites/abilities/AbilityHolder.java
@@ -0,0 +1,10 @@
+package org.zizitop.game.sprites.abilities;
+
+/**
+ * <br><br>
+ * Created 02.02.19 19:31
+ *
+ * @author Zizitop
+ */
+public class AbilityHolder {
+}
